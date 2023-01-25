0ff54f557bddb1e5b41742df13e26b381997621c
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-25 09:09:11 -0500

    add design login and use reactive forms

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private fb: FormBuilder) {}
 
+  formLogin = this.fb.group({
+    usuario: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  onLogin() {}
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, HomeComponent],
-  imports: [CommonModule, ComponentsModule],
+  imports: [CommonModule, ComponentsModule, ReactiveFormsModule],
   exports: [LoginComponent, HomeComponent],
 })
 export class PagesModule {}
